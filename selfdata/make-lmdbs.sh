95e54474deabf6068f3306034f07eb529bb27680
LuYuqiu <44193693+LuYuqiu@users.noreply.github.com>
2019-05-20 13:11:52 +0800

    1

--- selfdata/make-lmdbs.sh
+++
@@ -1,3 +0,0 @@
-../build/tools/convert_imageset_and_gt.bin staticbackground_chair/test_list.txt staticbackground_chair/staticbackground_TEST_lmdb 0 lmdb
-../build/tools/convert_imageset_and_gt.bin staticbackground_chair/train_list.txt staticbackground_chair/staticbackground_TRAIN_lmdb 0 lmdb
-
